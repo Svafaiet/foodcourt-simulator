3b11f7a36c5b8885ee4d39bf356d37c4e4878e5c
svafaiet <svafaiet@gmail.com>
2021-06-25 16:13:15 +0430

    Add customers

---
+++ customer.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+class Customer:
+    tiredness_rate = 1.0
+
+    @staticmethod
+    def get_tiredness_time():
+        return np.random.exponential(scale=1/Customer.tiredness_rate)
+
+    def __init__(self, star, service_type, start_time):
+        self.star = star
+        self.service_type = service_type
+        self.start_time = start_time
+        self.tired_time = start_time + Customer.get_tiredness_time()
+        self.paziresh_time = None
+        self.service_time = None
+        self.abandon_time = None
+
+    def is_tired(self, time):
+        return time > self.tired_time
