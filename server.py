643637cf134abaa6c4a167d5e09b6eb89e4443b5
svafaiet <svafaiet@gmail.com>
2021-06-25 18:24:25 +0430

    Add old updae

--- server.py
+++ server.py
@@ -3,15 +3,18 @@ from collections import deque
 import numpy as np
 import random
 
+from customer import Customer
+from queue import Queue
+
 
 class Server:
 
-    def __init__(self, averages, type_count):
+    def __init__(self, averages):
         self.availability = [0] * len(averages)
         self.services = list(list() for _ in range(len(self.availability)))
         self.averages = averages
-        self.queues = list(deque() for _ in range(type_count))
         self.clients = [None] * len(self.availability)
+        self.queue = Queue()
 
     def get_first_available(self):
         return min(self.availability)
@@ -28,5 +31,16 @@ class Server:
         self.availability[selected_index] = (time + service_time)
         self.services[selected_index].append(service_time)
 
+    def update(self, time=-1):
+        # print("TIME", time)
 
-
+        update_time = self.baje.get_first_available()
+        while (time == -1 or update_time <= time) and self.q:
+            customer_arrived = self.q.pop()
+            update_time = max(customer_arrived, self.baje.get_first_available())
+            wait_time = update_time - customer_arrived
+            # print("HAHA:", time, wait_time, customer_arrived, self.baje.availability)
+            if wait_time != 0:
+                self.waits.append(wait_time)
+            self.baje.reserve(update_time)
+            # print("UPDATE", update_time)
